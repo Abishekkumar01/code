bc510afe323f9eaeef82b992589960e9cf2ee361
Abishek <abishekkumarthurvas@gmail.com>
2025-04-04 07:11:28 -0700

    second_commit

--- name.py
+++ name.py
@@ -3,3 +3,5 @@ import numpy as np
 import matplotlib as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn .linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
