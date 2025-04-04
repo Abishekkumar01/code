8e1a99bee892f53206dd06a5968e4e77780f4634
Abishek <abishekkumarthurvas@gmail.com>
2025-04-04 07:49:52 -0700

    name

--- name.py
+++ name.py
@@ -7,4 +7,4 @@ from sklearn .linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.preprocessing import StandardScaler
